804cc530ebc935e98239f4067ad4922c859d4774
CodeMan <huseyinaltikulac@gmail.com>
2020-11-18 20:41:48 +0300

    Edittt

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use (express.static(__dirname+"/dist/"));
 
-app.get(/.*/,(req,res,next)=>{
+app.get("/",(req,res,next)=>{
     res.sendFile(__dirname+"/dist/index.html")
 })
 
