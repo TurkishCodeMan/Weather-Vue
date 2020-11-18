98ce2de57580bf45d695d31d95108e5b0c7a5fde
CodeMan <huseyinaltikulac@gmail.com>
2020-11-18 20:36:06 +0300

    Edit

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const path = require("path");
 const serveStatic = require("serve-static");
 const app = express();
 
-app.use ("/",serveStatic(path.join(__dirname,"/dist")));
+app.use (express.static(__dirname+"/dist/"));
+
+app.get(/.*/,(req,res,next)=>{
+    res.sendFile(__dirname+"/dist/index.html")
+})
 
 const port = process.env.PORT || 8080;
 
