04c182d580241478545c0f3d65ea0fb7a0f95a56
CodeMan <huseyinaltikulac@gmail.com>
2020-11-18 20:53:06 +0300

    Upgradeee

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const serveStatic = require("serve-static");
 const app = express();
 
-app.use ("/",serveStatic(path.join(__dirname,"/dist")));
+app.use ("/",serveStatic(path.join(__dirname,"/dist/")));
 
 
 const port = process.env.PORT || 8080;
