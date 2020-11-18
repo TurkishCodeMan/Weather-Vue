86928f983d9b4d727196e157ca4fc0d360f89f18
CodeMan <huseyinaltikulac@gmail.com>
2020-11-18 20:26:50 +0300

    Update Port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use ("/",serveStatic(path.join(__dirname,"/dist")));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port);
 console.log("Listening on port "+port);
