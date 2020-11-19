d34009fda176c30d7a5004f292f5cb3e4ca6d7d7
CodeMan <huseyinaltikulac@gmail.com>
2020-11-19 12:25:45 +0300

    Changeddd

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require("path");
 const serveStatic = require("serve-static");
 const app = express();
 
-app.use ("/",serveStatic(path.join(__dirname,"/dist")));
+app.get ("/",serveStatic(path.join(__dirname,"/dist")));
 
-app.get(/.*/,(req,res)=>{
+app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/dist/index.html"));
 })
 
